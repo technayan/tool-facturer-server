0a0f17aa3b33461c8b29a17ddbd2dd413201c494
dev_nayan <md.nayan8494@gmail.com>
2022-06-24 20:05:40 +0600

    Connect ot MongoDB and Create Products API

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +12,35 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('ToolFacturer server is running!');
 });
+
+
+// Connect to MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bbupgrs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run () {
+    try {
+        await client.connect();
+
+        const productCollection = client.db('tool-facturer').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const products = await productCollection.find(query).toArray();
+            res.send(products); 
+        })
+    }
+    finally {
+
+    }
+}
+
+run();
+
+
+
 
 app.listen(port, () => {
     console.log('Listening to the port', port);
