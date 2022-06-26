080c5b59b32f2f01f0d31ebda1cea4a94a59eb7c
dev_nayan <md.nayan8494@gmail.com>
2022-06-27 00:19:58 +0600

    All products API created

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,6 +14,21 @@ app.get('/', (req, res) => {
     res.send('ToolFacturer server is running!');
 });
 
+// Verify JWT Handler
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+        return res.status(401).send({message: 'Unauthorized Access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+        if(err) {
+            return res.status(403).send({message: 'Forbidden Access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 // Connect to MongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bbupgrs.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,12 +41,17 @@ async function run () {
         await client.connect();
 
         const productCollection = client.db('tool-facturer').collection('products');
+        const userCollection = client.db('tool-facturer').collection('users');
+        const orderCollection = client.db('tool-facturer').collection('orders');
 
+        // All Products API
         app.get('/products', async (req, res) => {
             const query = {}
             const products = await productCollection.find(query).toArray();
             res.send(products); 
-        })
+        });
+
+        
     }
     finally {
 
